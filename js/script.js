a217596d6a2447b104794d4d71cf8ac365950313
akamornikov <akamornikov@qulix.com>
2015-03-10 16:55:53 +0300

    Remove js logic for placeholder and some refactor

--- js/script.js
+++ js/script.js
@@ -10,11 +10,25 @@ function Todo() {
     var self = this;
 
     this.inputText = document.querySelector(".new-note");
-    this.inputText.style.fontStyle = TODO_APP.newTaskTextStyle;
 
     this.inputText.addEventListener("keydown", function(e) {
         if (e.keyCode === 13) {
-            
+            self.saveTasks();                 
         }
     });
+}
+
+Todo.prototype.saveTasks = function() {
+    var tasks = JSON.parse(localStorage.getItem("tasks")) || [],
+        newTask = new Task(this.inputText.value, false);
+    tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    tasks.forEach(function(cur) {
+        console.log(cur.text + " " + cur.isCompleted);
+    });
+};
+
+function Task(text, isCompleted) {
+    this.text = text;
+    this.isCompleted = isCompleted;
 }
