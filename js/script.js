4465e3610e66b2c83f9e126d0db4e3cb302dbcc1
akamornikov <akamornikov@qulix.com>
2015-03-10 18:04:13 +0300

    Code refactoring

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var TODO_APP = {    
-    newTaskTextStyle: "italic"
-};
-
 (function() {
     new Todo();
 }());
@@ -13,14 +9,15 @@ function Todo() {
 
     this.inputText.addEventListener("keydown", function(e) {
         if (e.keyCode === 13) {
-            self.saveTasks();                 
+            self.addTask(self.inputText.value); 
+            self.inputText.value = "";               
         }
     });
 }
 
-Todo.prototype.saveTasks = function() {
+Todo.prototype.addTask = function(taskText) {
     var tasks = JSON.parse(localStorage.getItem("tasks")) || [],
-        newTask = new Task(this.inputText.value, false);
+        newTask = new Task(taskText, false);
     tasks.push(newTask);
     localStorage.setItem("tasks", JSON.stringify(tasks));
     tasks.forEach(function(cur) {
