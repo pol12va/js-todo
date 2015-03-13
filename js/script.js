0ed19f9dd38c2d1120871921e49642bd6e4a9847
akamornikov <akamornikov@qulix.com>
2015-03-13 15:24:57 +0300

    Make methods of TODO private

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function Todo() {
+    if (!(this instanceof Todo)) {
+        return new Todo();
+    }
     var self = this,
         tasksJson = localStorage.getItem("tasks"),
         addTask,
         inputText = document.querySelector(".new-note");
 
     this.tasks = (tasksJson !== null && tasksJson !== []) ? JSON.parse(tasksJson) : [];
-
     if (this.tasks.length !== 0) {
         this.tasks.forEach(function(cur) {
             self.addTask(cur);
@@ -23,28 +25,34 @@ function Todo() {
     });
 }
 
-Todo.prototype.addTask = function(task) {
-    var checkbox = document.createElement("input"),
-        taskRow = document.createElement("div"),
-        label = document.createElement("label"),
-        taskList = document.querySelector(".task-list");
+Todo.prototype = (function() {
+    return {
+        addTask: function(task) {
+            var checkbox = document.createElement("input"),
+                taskRow = document.createElement("div"),
+                label = document.createElement("label"),
+                taskList = document.querySelector(".task-list");
 
-    checkbox.type = "checkbox";
-    checkbox.className = "toggle-task";
-    taskList.lastChild ? label.className = "task" : label.className = "last-task";
-    label.textContent = task.text;
-    taskRow.appendChild(label);
-    taskRow.appendChild(checkbox);
-    taskList.insertBefore(taskRow, taskList.firstChild);
-};
-
-Todo.prototype.loadTasks = function() {
-    this.tasks.forEach(function(cur) {
-        this.addTask(cur);
-    }.bind(this));
-};
+            checkbox.type = "checkbox";
+            checkbox.className = "toggle-task";
+            taskList.lastChild ? label.className = "task" : label.className = "last-task";
+            label.textContent = task.text;
+            taskRow.appendChild(label);
+            taskRow.appendChild(checkbox);
+            taskList.insertBefore(taskRow, taskList.firstChild);
+        },
+        loadTasks: function() {
+            this.tasks.forEach(function(cur) {
+                this.addTask(cur);
+            }.bind(this));    
+        }
+    };    
+}());
 
 function Task(text, isCompleted) {
+    if (!(this instanceof Task)) {
+        return new Task();
+    }
     this.text = text;
     this.isCompleted = isCompleted;
 }
